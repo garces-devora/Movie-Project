0c0073fb92cca6520b4b88d1bd9ece4c84e4e499
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-22 11:00:07 -0500

    Created add movie function and connected to submit button

--- movie-project.js
+++ movie-project.js
@@ -1,8 +1,13 @@
 "use script";
 
 let refresh = $(`#reload`).toggleClass(`hidden`);
+let newTitle = document.getElementById("add-movie");
+let newRating = document.getElementById("rating");
+let submitNewMovie = document.querySelector("#submit-movie")
 
 getMovies();
+
+// Retrieve movies from db.JSON and display on html
 function getMovies() {
 
 $(`#reload`).toggleClass(`hidden`)
@@ -17,7 +22,6 @@ fetch('https://pollen-pumped-brace.glitch.me/movies')
 
         let html="";
 
-
             for (let i = 0; i <data.length; i ++) {
             // console.log(data[i].title);
                 html += `<div class="">`
@@ -25,8 +29,8 @@ fetch('https://pollen-pumped-brace.glitch.me/movies')
                 html += `<div class="card-body">`
                 html += `<h5 class="card-title">Movie Title: ${data[i].title}</h5>`
                 html += `<h6 class="card-text">Rating: ${data[i].rating}</h6>`
-                html += `<h6 class="card-text">Director: ${data[i].director}</h6>`
-                html += `<h6 class="card-text">Genre: ${data[i].genre}</h6>`
+                // html += `<h6 class="card-text">Director: ${data[i].director}</h6>`
+                // html += `<h6 class="card-text">Genre: ${data[i].genre}</h6>`
                 html += `</div>`
 
             }
@@ -36,3 +40,24 @@ fetch('https://pollen-pumped-brace.glitch.me/movies')
     )
 }
 
+// Function to add movies
+
+function newMovie(x) {
+    x.preventDefault()
+    let movieObj ={
+        title: newTitle.value,
+        rating: newRating.value
+    }
+
+    // Fetch request to POST to movie list
+    fetch('https://pollen-pumped-brace.glitch.me/movies', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieObj),
+    }).then(() => fetch('https://pollen-pumped-brace.glitch.me/movies')).then(resp => resp.json()).then(() => getMovies());
+    newTitle.value = "";
+}
+
+submitNewMovie.addEventListener('click', newMovie)
